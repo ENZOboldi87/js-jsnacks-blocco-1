140fefe77b8bc24b95d21ffa5c51db3645c33a24
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-06 15:57:43 +0200

    esercizio 8 e 9 completato

--- Esercizio 8/js/myscript.js
+++ Esercizio 8/js/myscript.js
@@ -2,3 +2,26 @@
 // Genera N array, ognuno formato da 10 numeri casuali
 // tra 1 e 100.
 // Ogni volta che ne crei uno, stampalo a schermo.
+
+
+// devo far inserire un numero n all utente
+var numUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
+
+// devo generare un array di 10 numeri casuali a 1 a 100
+for (var i = 0; i < numUtente; i++) {
+  var arrayGenerato = generaArray();
+  console.log(arrayGenerato);
+
+}
+// stampare l array
+
+
+// funzione per generare array casuali
+function generaArray() {
+  var arrayGenerato = [];
+  for (var i = 0; i < 10; i++) {
+    var numeroCasuale = Math.floor(Math.random() * 100) + 1;
+    arrayGenerato.push(numeroCasuale);
+  }
+  return arrayGenerato;
+}
--- Esercizio 9/js/myscript.js
+++ Esercizio 9/js/myscript.js
@@ -2,3 +2,23 @@
 // colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i
 // numeri dispari e in verde i numeri pari.
+
+// variabili
+var boxRosso = document.getElementById('rosso');
+var boxVerde = document.getElementById('verde')
+
+ // devo creare un array di numeri
+ var arrayNumeri = [1, 2, 4, 6, 10, 3, 4, 67, 78, 87, 98, 45, 20];
+// ciclo for per leggere l array
+ for (var i = 0; i < arrayNumeri.length; i++) {
+ // se il numero e pari
+ if (arrayNumeri[i] % 2===0) {
+   var numeriPari = arrayNumeri[i];
+   boxVerde.innerHTML = boxVerde.innerHTML + ' ' + numeriPari;
+ }
+ // se il numero e dispari
+ else {
+   var numeriDispari = arrayNumeri[i];
+   boxRosso.innerHTML =  boxRosso.innerHTML + ' ' + numeriDispari;
+  }
+ }
